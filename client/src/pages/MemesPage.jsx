cc30518631bd3ed93f84a23bfa84e31d3d39102b
Victor <vhessevaagbakke@gmail.com>
2026-05-23 15:11:51 +0200

    små forandringer: la til bilde som variabel, endret litt på en knapp og fjernet span fra tittelen

--- client/src/pages/MemesPage.jsx
+++ client/src/pages/MemesPage.jsx
@@ -1,4 +1,24 @@
 import { useState } from 'react'
 import Masthead from '../components/Masthead.jsx'
 import '../styles/np-front-page.css'
-<p>hello world</p>
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { fetchArticleById } from '../services/articleService'
+
+
+function MemesPage() {
+    const [activeSection] = useState('Memes')
+    const handleSectionSelect = (section) => {
+        if (section === 'Memes') {
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        }          
+    }
+
+    return (
+        <div className="np-page-shell">
+            <Masthead onSectionSelect={handleSectionSelect} activeSection={activeSection} />    
+            <main className="np-main" id="main-content">
+                <p>hello world</p>
+            </main>
+        </div>
+    )
+}
