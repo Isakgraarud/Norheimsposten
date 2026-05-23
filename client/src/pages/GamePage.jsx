d3d560988658aee642f0f500e7fee93ce65007bd
Victor <vhessevaagbakke@gmail.com>
2026-05-23 14:49:27 +0200

    paging og redirect funker nå

---
+++ client/src/pages/GamePage.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+import Masthead from '../components/Masthead.jsx'
+import '../styles/np-front-page.css'
+
+
+<p>hello world</p>
---
+++ client/src/pages/MemesPage.jsx
@@ -0,0 +1,4 @@
+import { useState } from 'react'
+import Masthead from '../components/Masthead.jsx'
+import '../styles/np-front-page.css'
+<p>hello world</p>
